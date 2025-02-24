6766fd7ba8ddbfa714a90b4d6ac858116bb8c7c6
shahzad2121 <shahzadnadeem.dev@gmail.com>
2025-02-24 05:31:02 +0500

    userSchema updated

---
+++ Backend/src/models/user/user.mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+
+const userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      minlength: [3, "name should be of 3 character"],
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: [5, "Email must be contain on 5 characters"],
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+      minlength: [6, "Password must of 6 characters"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET || "your_security", {
+    expiresIn: "24h",
+  });
+  return token;
+};
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;

0a5b12bbfdf284afa1a5125d5f2a9e8d3945fdd5
shahzad2121 <shahzadnadeem.dev@gmail.com>
2025-02-26 04:25:48 +0500

    Routes and context etc setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Welcome from "./pages/Welcome";
+import { Route, Router, Routes } from "react-router-dom";
+import Start from "./pages/Start";
+import UserLogin from "./pages/user/UserLogin";
+import UserSignup from "./pages/user/UserSignup";
+import CaptainLogin from "./pages/captain/CaptainLogin";
+import CaptainSignup from "./pages/captain/CaptainSignup";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        <Route path="/" element={<Start />} />
+        <Route path="/user-login" element={<UserLogin />} />
+        <Route path="/user-signup" element={<UserSignup />} />
+        <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/captain-signup" element={<CaptainSignup />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </>
   );
--- client/src/pages/Start.jsx
+++ client/src/pages/Start.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const Start = () => {
   return (
-    <div>Start</div>
-  )
-}
+    <div>
+      <div className='w-screen h-[70vh] bg-[url("https://images.unsplash.com/photo-1588260369134-d64f66c5730b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHRyYWZmaWMlMjBsaWdodHxlbnwwfHwwfHx8Mg%3D%3D")] bg-center bg-no-repeat bg-cover text-2xl text-black font-medium pl-5 pt-5'>
+        Uber
+      </div>
+      <div className="px-5">
+        <h3 className="mt-3 mb-5 font-semibold text-2xl">
+          Get started with Uber
+        </h3>
+        <Link
+          to="/user-login"
+          className="bg-black mb-3 text-white rounded-md items-center justify-between pl-33 pr-5 flex py-2"
+        >
+          <span className="font-light text-base">As a User</span>
 
-export default Start
+          <FaArrowRightLong />
+        </Link>
+        <Link
+          to="/captain-login"
+          className=" bg-zinc-600 text-white rounded-md items-center justify-between pl-33 pr-5 flex py-2"
+        >
+          <span className="font-light text-base">As a Captain</span>
+
+          <FaArrowRightLong />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Start;

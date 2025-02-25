eb6fa8575d97b68a1124dd0c64e183e31eae3c2b
shahzad2121 <shahzadnadeem.dev@gmail.com>
2025-02-26 04:27:30 +0500

    user conxt setup

---
+++ client/src/context/UserContext.jsx
@@ -0,0 +1,15 @@
+import React, { createContext, useState } from "react";
+
+export const userDataContext = createContext();
+
+const UserContext = ({ children }) => {
+  const [user, setUser] = useState({});
+
+  return (
+    <userDataContext.Provider value={[user, setUser]}>
+      {children}
+    </userDataContext.Provider>
+  );
+};
+
+export default UserContext;

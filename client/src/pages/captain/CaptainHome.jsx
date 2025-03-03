28011db4088bbb39df15baeabde0eefcd949e209
shahzad2121 <shahzadnadeem.dev@gmail.com>
2025-03-03 21:55:50 +0500

    captain home updated

--- client/src/pages/captain/CaptainHome.jsx
+++ client/src/pages/captain/CaptainHome.jsx
@@ -1,11 +1,75 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import CaptainLogout from "./CaptainLogout";
+import { Link } from "react-router-dom";
+import CaptainDetails from "../../components/captain/CaptainDetails";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import ConfirmRidePopUp from "../../components/captain/ConfirmRidePopUp";
+import RidePopUp from "../../components/captain/RidePopUp";
 
 const CaptainHome = () => {
+  const [ ridePopupPanel, setRidePopupPanel ] = useState(true)
+  const [ confirmRidePopupPanel, setConfirmRidePopupPanel ] = useState(false)
+
+  const ridePopupPanelRef = useRef(null)
+  const confirmRidePopupPanelRef = useRef(null)
+  const [ ride, setRide ] = useState(null)
+
+
+  useGSAP(function () {
+    if (ridePopupPanel) {
+        gsap.to(ridePopupPanelRef.current, {
+            transform: 'translateY(0)'
+        })
+    } else {
+        gsap.to(ridePopupPanelRef.current, {
+            transform: 'translateY(100%)'
+        })
+    }
+}, [ ridePopupPanel ])
+
+useGSAP(function () {
+    if (confirmRidePopupPanel) {
+        gsap.to(confirmRidePopupPanelRef.current, {
+            transform: 'translateY(0)'
+        })
+    } else {
+        gsap.to(confirmRidePopupPanelRef.current, {
+            transform: 'translateY(100%)'
+        })
+    }
+}, [ confirmRidePopupPanel ])
+
+
   return (
-    <div>
-      <CaptainLogout />
-    </div>
+    <div className='h-screen'>
+            <div className='fixed p-6 top-0 flex items-center justify-between w-screen'>
+                <img className='w-16' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+                <Link to='/captain/home' className=' h-10 w-10 bg-white flex items-center justify-center rounded-full'>
+                    <i className="text-lg font-medium ri-logout-box-r-line"></i>
+                </Link>
+            </div>
+            <div className='h-3/5'>
+                <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
+
+            </div>
+            <div className='h-2/5 p-6'>
+                <CaptainDetails />
+            </div>
+            <div ref={ridePopupPanelRef} className='fixed w-full z-10 bottom-0 translate-y-0 bg-white px-3 py-10 pt-12'>
+                <RidePopUp
+                    ride={ride}
+                    setRidePopupPanel={setRidePopupPanel}
+                    setConfirmRidePopupPanel={setConfirmRidePopupPanel}
+                    // confirmRide={confirmRide}
+                />
+            </div>
+            <div ref={confirmRidePopupPanelRef} className='fixed w-full h-screen z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
+                <ConfirmRidePopUp
+                    ride={ride}
+                    setConfirmRidePopupPanel={setConfirmRidePopupPanel} setRidePopupPanel={setRidePopupPanel} />
+            </div>
+        </div>
   );
 };
 

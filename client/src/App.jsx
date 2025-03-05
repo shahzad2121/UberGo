000f9378b782b50c5239f73860902a02069c5a1e
shahzad2121 <shahzadnadeem.dev@gmail.com>
2025-03-05 23:25:53 +0500

    App updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,9 @@ import CaptainProtectWrapper from "./pages/captain/CaptainProtectWrapper";
 import CaptainHome from "./pages/captain/CaptainHome";
 import UserLogOut from "./pages/user/UserLogOut";
 import CaptainLogout from "./pages/captain/CaptainLogout";
+import RideStarted from "./components/user/RideStarted";
+import RideMoved from "./pages/captain/RideMoved";
+
 
 const App = () => {
   return (
@@ -19,8 +22,11 @@ const App = () => {
         <Route path="/" element={<Start />} />
         <Route path="/user/login" element={<UserLogin />} />
         <Route path="/user/signup" element={<UserSignup />} />
+        <Route path="/user/ride" element={<RideStarted />} />
         <Route path="/captain/login" element={<CaptainLogin />} />
         <Route path="/captain/signup" element={<CaptainSignup />} />
+        <Route path="/captain/ride" element={<RideMoved />} />
+
         <Route
           path="/user/home"
           element={

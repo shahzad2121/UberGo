dca72fc2a05ba822c0d9eb34731f1389441ab10d
shahzad2121 <shahzadnadeem.dev@gmail.com>
2025-03-08 21:02:56 +0500

    ride controller created

---
+++ server/src/routes/ride/ride.controller.js
@@ -0,0 +1,22 @@
+const { validationResult } = require("express-validator");
+const { createRide } = require("../../services/ride.services");
+
+async function httpCreateRide(req, res) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const ride = req.body;
+    const newRide = await createRide(ride);
+
+    return res.status(201).json(newRide);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+}
+
+module.exports = {
+  httpCreateRide,
+};
